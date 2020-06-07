05d3e479763dd25ed250564a30b2d7be44f72679
gagarin2017 <yury_gagarin2017@mail.ru>
2020-06-07 13:54:38 +0100

    -react talking to remote web server - axios

--- React - The Complete Guide/http--01-starting-setup/src/index.js
+++ React - The Complete Guide/http--01-starting-setup/src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import Axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+Axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+Axios.defaults.headers.common["Authorization"] = "AUTH_TOKEN";
+Axios.defaults.headers.post["Content-Type"] = "application/json";
+
+Axios.interceptors.request.use(
+  (request) => {
+    console.log("request", request);
+    // Edit request config
+    return request;
+  },
+  (error) => {
+    console.log("error", error);
+    Promise.reject(error);
+  }
+);
+
+Axios.interceptors.response.use(
+  (response) => {
+    console.log("response", response);
+    // Edit request config
+    return response;
+  },
+  (error) => {
+    console.log("error", error);
+    Promise.reject(error);
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
